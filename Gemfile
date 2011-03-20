d18c34b5e41a0c295e05b1c1b02ab3b132bdc5f9
Mariusz Lusiak <mariusz@applicake.com>
2011-03-20 10:52:35 +0100

    Creates app scaffold

---
+++ Gemfile
@@ -0,0 +1,12 @@
+source 'http://rubygems.org'
+
+gem "rails"
+
+group :develpoment do
+  gem "rspec-rails"
+  gem "capybara"
+  gem "launchy"
+  gem "interactive_editor"
+  gem "awesome_print"
+  gem "ruby-debug19"
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WorkshopTest::Application.config.secret_token = 'a270245e0543443851233fa29a2b2d713cf0c30cfeda56c55f649076eca66ef31d783a4024570f6edb0255ffdceb636c77aa902e0884146707ed2067e0064420'
